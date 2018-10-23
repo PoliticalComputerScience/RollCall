602767c6c7dd8eac6837d1a117724bf33555647c
JasonHuynh <jasonhuynh73@berkeley.edu>
2018-10-23 16:55:15 -0700

    fixed some bugs with senate, updated API key

--- get_vote_data.py
+++ get_vote_data.py
@@ -2,14 +2,17 @@ from utils import get_json
 import json, csv
 from datetime import date
 from math import sqrt
+import pandas as pd
+import itertools as it
 CHAMBERS = ["both", "house", "senate"]
 CURR_CONGRESS_START = "2017-01-03"
 
-""" Converts (and filters) a list of votes into a list of URI's for the votes corresponding to bills. """
+""" Converts (and filters) a list of votes into a list of URI's for the votes corresponding to bills.
+    TODO: Filter out the roll calls that we care about. """
 def filter_uri(votes):
     uri_list = []
     for v in votes:
-        if (("bill" in v) and (v["question"] == "On Passage")):
+        if (("bill" in v) and ("On Passage" in v["question"])):
             uri_list.append(v["vote_uri"])
     return uri_list
 
@@ -36,6 +39,7 @@ def get_vote_roll_call(start_date, end_date, chamber="both"):
     uri_list = filter_uri(vote_info["votes"])
     return uri_to_roll_call(uri_list)
 
+""" Helper function for get_year_vote_roll_call() """
 def get_year_vote_info(year=2018, chamber="both"):
     assert type(year) == int and year <= 2018 and year > 1900 and chamber in CHAMBERS
     year = str(year)
@@ -89,4 +93,63 @@ def extract_processed_data(file_name):
     return votes
 
 
+""" Constructs a dictionary that maps congressmembers to how they voted on certain bills"""
+def get_member_votes(congress, chamber="both"):
+  big_boy = get_congress_vote_roll_call(congress, chamber)
+  my_dict = {}
 
+  for tuple1 in big_boy:
+
+    bill_ID = tuple1['bill_id']
+    positions = tuple1['positions']
+
+    for member_position in positions:
+
+      member_id = member_position["member_id"]
+      member_vote = member_position["vote_position"]
+
+      if (member_id not in my_dict.keys()):
+          my_dict[member_id] = []
+      if (member_vote != "Not Voting"): #Filter out abstaining Congress members
+          my_dict[member_id] += [(bill_ID, member_vote)]
+
+  return my_dict
+
+
+
+""" Constructs a set of all combinations of 2 members, storing:
+		1) How many bills they voted the same on
+		2) How many bills they both voted on
+    Should call on either Senate or House, both may not be entirely useful.
+"""
+def pair_similarity(congress, chamber='both'):
+	member_pairs = set()
+	member_votes = get_member_votes(congress, chamber)
+
+	for m in it.combinations(member_votes.keys(), 2):
+
+		if m[0] == m[1]:
+			continue
+		#[(bill_id, vote), (bill_id, vote)...]
+		m1 = member_votes[m[0]]
+		m2 = member_votes[m[1]]
+
+		# tables of [(bill_id, vote),(bill_id, vote) ...]
+		d1 = pd.DataFrame(m1, columns=['bill_id1','vote1'])
+		d2 = pd.DataFrame(m2, columns=['bill_id2','vote2'])
+
+		d3 = d1.merge(d2, how="inner", left_on='bill_id1', right_on='bill_id2')
+		vs = d3.loc[d3['vote1'] == d3['vote2'],:].shape[0]
+		vt = d3.shape[0]
+
+		ntuple = (m[0], m[1], vs, vt)
+		member_pairs.add(ntuple)
+
+	# store_as_csv(member_pairs)
+
+	return member_pairs
+
+
+# def store_as_csv(pairs)
+# 	dataframee = pd.DataFrame(pairs, columns=['member_1', 'member_2', 'votes_same',''])
+# 	dataframee.to_csv('Pairs')
--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+""" This file contains get_json(api_url) which returns the dictionary form of
+JSON object returned by the ProPublica API. """
+
 from urllib.request import Request, urlopen
 import json
 API_KEY = "dtoa6UrIaAQBqiXOdBPmFeS36VyVxxorxqU4mXkv"
@@ -8,7 +11,7 @@ def get_json(api_url):
 	query = Request(api_url)
 	query.add_header("X-API-KEY", API_KEY)
 	res = json.loads(urlopen(query).read().decode("utf-8"))
-	if res["status"] != "OK": 
+	if res["status"] != "OK":
 		print("invalid query: " + res["status"])
 		return None
 	return res
