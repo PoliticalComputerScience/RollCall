ba86e2896eba5366a627ebbf0cc6971c4ace4ef1
JasonHuynh <jasonhuynh73@berkeley.edu>
2018-10-23 19:53:24 -0700

    jason's commit

--- metrics.py
+++ metrics.py
@@ -1,2 +1,13 @@
+from utils import *
 def weighted_sum(m1, m2, w1, w2):
-  return w1*m1 + w2*m2
+    return w1*m1 + w2*m2
+
+""" Calculates the metric with the given weighted sums. """
+def metric(filename, a, b):
+    data = from_csv(filename)
+    naive = naive_metric(data)
+    sponsorship = normalize(data)
+    pairs = []
+    for i in range(len(data)):
+        pairs.append((data[i][0], data[i][1], weighted_sum(naive[i][2], sponsorship[i][2], a, b)))
+    return pairs
