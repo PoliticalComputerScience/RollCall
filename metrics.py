f60f359064b05b262a1a89a1b2f9525d2f54f5ad
Aaron Li <jili68@berkeley.edu>
2018-10-23 19:51:53 -0700

    normalize function implementation

--- metrics.py
+++ metrics.py
@@ -1,2 +1,11 @@
+import * from statistics
+
 def weighted_sum(m1, m2, w1, w2):
   return w1*m1 + w2*m2
+
+def normalize(input_list):
+    temp = [x[4] for x in input_list]
+    mid = median(temp)
+    new_list = [k / mid for k in temp]
+    result = [(input_list[y][0], input_list[y](1), input_list[y](2), input_list[y](3), new_list[y]) for y in range(len(input_list))]
+    return result
