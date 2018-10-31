5ce3b9ebd70a7b649051bde85216d8948e8af933
JasonHuynh <jasonhuynh73@berkeley.edu>
2018-10-30 19:12:43 -0700

    more metrics stuff

--- metrics.py
+++ metrics.py
@@ -4,19 +4,31 @@ import * from statistics
 def weighted_sum(m1, m2, w1, w2):
     return w1*m1 + w2*m2
 
-""" Calculates the metric with the given weighted sums. """
-def metric(filename, a, b):
+""" Calculates the metric using data from <filename> with the given weighted sums. """
+def metric(filename, naive_weight, sponsor_weight):
     data = from_csv(filename)
     naive = naive_metric(data)
-    sponsorship = normalize(data)
+    sponsorship = sponsorship_metric(data)
     pairs = []
     for i in range(len(data)):
-        pairs.append((data[i][0], data[i][1], weighted_sum(naive[i][2], sponsorship[i][2], a, b)))
+        pairs.append((data[i][0], data[i][1], weighted_sum(naive[i][2], sponsorship[i][2], naive_weight, sponsor_weight)))
     return pairs
 
-def normalize(input_list):
+def naive_metric(tuples):
+	lst = []
+	for t in tuples:
+		lst = lst.append((t[0] , t[1] , t[2]/t[3]))
+	return lst
+
+def sponsorship_metric(input_list):
     temp = [x[4] for x in input_list]
     mid = median(temp)
     new_list = [k / mid for k in temp]
     result = [(input_list[y][0], input_list[y](1), input_list[y](2), input_list[y](3), new_list[y]) for y in range(len(input_list))]
     return result
+
+""" Calculates metric and writes a list of tuples (member_a, member_b, ) """
+def calculate_metric(chamber, congress, naive_weight, sponsor_weight):
+    filename = chamber + '_' + congress + '.csv'
+    data = metric(filename, naive_weight, sponsor_weight)
+    to_csv(('member_a', 'member_b', 'metric'), data, chamber + '_' + congress + '_metric.csv')
