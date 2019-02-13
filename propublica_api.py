22bc0246899f9e5f19f1e6fd2b7c65b1ecf58e88
JasonHuynh <jasonhuynh73@berkeley.edu>
2019-02-12 21:25:33 -0800

    renamed api.py to propublica_api.py

--- propublica_api.py
+++ propublica_api.py
@@ -8,7 +8,8 @@ CHAMBERS = ["both", "house", "senate"]
 CURR_CONGRESS_START = "2017-01-03"
 
 """
-Creates and returns a dictionary with pairs as values and their hashes as keys.
+    Creates and returns a dictionary with pairs of congressmembers as keys and
+    their data dictionaries as values.
 """
 def get_pairs(congress, chamber):
     uri = "https://api.propublica.org/congress/v1/{}/{}/members.json".format(congress, chamber)
@@ -19,11 +20,14 @@ def get_pairs(congress, chamber):
             pair = Pair(members[i]['id'], members[j]['id'], \
                         members[i]['first_name'] + ' ' + members[i]['last_name'], \
                         members[j]['first_name'] + ' ' + members[j]['last_name'])
-            pairs[hash(pair)] = pair
+            pairs[pair] = pair.data
     return pairs
 
-""" Converts (and filters) a list of votes into a list of URI's for the votes corresponding to bills.
-    TODO: Filter out the roll calls that we care about. """
+"""
+    Converts (and filters) a list of votes into a list of URI's for the votes
+    corresponding to bills.
+    TODO: Filter out the roll calls that we care about.
+"""
 def filter_uri(votes):
     uri_list = []
     for v in votes:
@@ -31,7 +35,10 @@ def filter_uri(votes):
             uri_list.append(v['vote_uri'])
     return uri_list
 
-""" Converts a list of URI's into a list of {'bill_id':bill_id, 'positions':[pos0, pos1, ...]} corresponding to each URI. """
+"""
+    Converts a list of URI's into a list of {'bill_id':bill_id, 'positions':
+    [pos0, pos1, ...]} corresponding to each URI.
+"""
 def uri_to_roll_call(uri_list):
     roll_calls = []
     for u in uri_list:
@@ -42,7 +49,9 @@ def uri_to_roll_call(uri_list):
         roll_calls.append(d)
     return roll_calls
 
-""" Helper function for get_year_vote_roll_call() """
+"""
+    Helper function for get_year_vote_roll_call()
+"""
 def get_year_vote_info(year=2018, chamber="both"):
     assert type(year) == int and year <= 2018 and year > 1900 and chamber in CHAMBERS
     year = str(year)
@@ -58,7 +67,9 @@ def get_year_vote_info(year=2018, chamber="both"):
         month_to_vote_info[i] = month_vote_info["results"]
     return month_to_vote_info
 
-""" Gets the roll call information for the votes in the given year using get_year_vote_info """
+"""
+    Gets the roll call information for the votes in the given year using get_year_vote_info
+"""
 def get_year_vote_roll_call(year=2018, chamber="both"):
     vote_info = get_year_vote_info(year, chamber)
     uri_list = []
@@ -67,8 +78,8 @@ def get_year_vote_roll_call(year=2018, chamber="both"):
     return uri_to_roll_call(uri_list)
 
 """
-Gets the roll call and sponsorship information for the votes in the given congress.
-Returns of list of dictionarys {'bill_id': bill_id, 'positions': positions, 'sponsor_list': sponsor_list}
+    Gets the roll call and sponsorship information for the votes in the given congress.
+    Returns of list of dictionarys {'bill_id': bill_id, 'positions': positions, 'sponsor_list': sponsor_list}
 """
 def get_congress_vote_roll_call(congress, chamber="both"):
     offset = 115 - congress
@@ -87,11 +98,11 @@ def get_congress_vote_roll_call(congress, chamber="both"):
     return roll_call_list
 
 
-""" Constructs a dictionary that maps congressmembers to how they voted on certain bills
-@Param
+"""
+    Constructs a dictionary that maps congressmembers to how they voted on certain bills
+    @Param
 """
 def get_member_votes(big_boy):
-  #big_boy = get_congress_vote_roll_call(congress, chamber)
 
   my_dict = {}
 
@@ -113,25 +124,23 @@ def get_member_votes(big_boy):
   return my_dict
 
 
-""" Constructs a set of all combinations of 2 members, storing:
+"""
+    Constructs a set of all combinations of 2 members, storing:
 		1) How many bills they voted the same on
 		2) How many bills they both voted on
     Should call on either Senate or House, both may not be entirely useful.
     Returns a list of tuples (member0, member1, votes_same, bills_same)
 """
 def pair_similarity(member_votes, pairs):#congress, chamber='both'):
-	#member_pairs = set()
-	#member_votes = get_member_votes(congress, chamber)
 
     for m in it.combinations(member_votes.keys(), 2):
 
         if m[0] == m[1]:
             continue
-		#[(bill_id, vote), (bill_id, vote)...]
+
         m1 = member_votes[m[0]]
         m2 = member_votes[m[1]]
 
-		# tables of [(bill_id, vote),(bill_id, vote) ...]
         d1 = pd.DataFrame(m1, columns=['bill_id1','vote1'])
         d2 = pd.DataFrame(m2, columns=['bill_id2','vote2'])
 
@@ -139,17 +148,13 @@ def pair_similarity(member_votes, pairs):#congress, chamber='both'):
         vs = d3.loc[d3['vote1'] == d3['vote2'],:].shape[0]
         vt = d3.shape[0]
 
-        #ntuple = (m[0], m[1], vs, vt)
-        #member_pairs.add(ntuple)
-        hash = get_pair_hash(m[0], m[1])
+        dummy_pair = Pair(m[0], m[1]) #used to access dictionary
         try:
-            pairs[hash].data['votes_same'] = vs
-            pairs[hash].data['votes_total'] = vt
+            pairs[dummy_pair]['votes_same'] = vs
+            pairs[dummy_pair]['votes_total'] = vt
         except KeyError:
             print("Discrepancy in ProPublica member data")
 
-
-	#return member_pairs
 
 """
 Aggregates pairs for the congress and chamber, writing data to the file
@@ -160,23 +165,24 @@ def write_pairs(congress, chamber='both'):
     roll_calls = get_congress_vote_roll_call(congress, chamber)
     member_votes = get_member_votes(roll_calls)
     pair_similarity(member_votes, pairs)#congress, chamber)
-    for hash, pair in pairs.items():
+    #adding mutual sponsorship data to pair data dictionaries
+    for pair in pairs.keys():
         for roll_call in roll_calls:
             if (pair.id_a in roll_call['sponsor_list'] and \
             pair.id_b in roll_call['sponsor_list']):
                 pair.data['mutual_sponsorships'] += 1
     #You can do further processing to the pairs here
 
-    new_pairs = [v.toTuple() for k, v in pairs.items()]
+    new_pairs = [k.toTuple() for k in pairs.keys()]
     to_csv(['member_a_id', 'member_b_id', 'member_a_name', 'member_b_name', \
         'votes_same', 'bills_same', 'mutual_sponsorships'], new_pairs, \
         str(congress) + '_' + chamber + '.csv')
 
 if __name__ == "__main__":
-    sessions = [109, 110, 112, 113, 114, 115]
-    for session in sessions[4:]:
+    sessions = [109]
+    for session in sessions:
         print('Session: ' + str(session))
         try:
-            write_pairs(session, 'house')
+            write_pairs(session, 'senate')
         except KeyError:
             print("key error")
